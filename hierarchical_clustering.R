5d257125adad95f1a26a097fcbe5f694f990545a
Pablo-JiFe <pablojimenez@lasallistas.org.mx>
2026-07-16 15:06:09 -0600

    script for hierarchical clustering based on the boruta obtained gene list

---
+++ hierarchical_clustering.R
@@ -0,0 +1,64 @@
+library(factoextra)
+
+# List obtained from Boruta
+
+gene_list <- c("APEX2", "ARHGAP1", "ARHGEF39", "CCDC97", "CGREF1", "CLUAP1", "COL22A1", "CPE", "CTNNBIP1", "CYFIP1", "DHRS11", "DLX1", "ERCC4", "F13A1", "FAM110D", "FAT1", "FKBP11", "GALNT14", "GBP1", "GMIP", "GRAMD1B", "HSD11B2", "INPP4A", "KERA", "KIF25", "LGR6", "LURAP1L", "MEF2A", "MRTFB", "MXI1", "NUBP1", "SF3B3", "SLC12A4", "SLC45A4", "SLC8A3", "STAT5B", "TIMM50", "TPD52", "TRIM68", "TSHZ3", "UBE2D4", "UNC5B", "VMP1")
+
+# Keep only genes for clustering
+
+counts_data_hc <- counts_data[rownames(counts_data) %in% gene_list, ]
+
+# Transpose so pt in rows and geenes in columns
+
+counts_data_hc <-
+  counts_data_hc %>%
+  t()
+
+# Clustering ----------------------------------------------------------
+
+fviz_nbclust(counts_data_hc, FUN = hcut, method = "silhouette")
+
+# Distance matrix between samples
+
+dist_microarray.brca <- get_dist(counts_data_hc, method = "euclidean")
+
+# Clustering 
+
+hc.out_microarray.brca <- hclust(dist_microarray.brca, method = "ward.D2")
+
+# Clustering characteristics ------------------------------------------
+
+# Dendrogram
+
+plot(
+  hc.out_microarray.brca,
+  labels = FALSE,
+  hang = -1,
+  main = "CLUSTERING JERÁRQUICO METABRIC"
+)
+
+rect.hclust(hc.out_microarray.brca,
+            k = 2,
+            border = "purple") # bottom up approach
+
+# Tree of clusters
+
+clusters <- cutree(hc.out_microarray.brca, k = 2)
+
+# Observe how many patients in each cluster
+
+table(clusters)
+
+# Create object as data frame
+
+cluster_df <- data.frame(clusters = clusters)
+
+# Asign a column named sample with the rownames such that we can then merge based on that column
+
+cluster_df$sample <- rownames(counts_data_hc)
+
+# Merge object so that in the metadata there is a column corresponding to that patients cluster
+
+metadata_os <- 
+  metadata_os %>% 
+  left_join(cluster_df, by = "sample")
