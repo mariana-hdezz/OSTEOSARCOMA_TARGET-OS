34f7988e4c9db8084112557cb6b9e2f38b894896
Pablo-JiFe <pablojimenez@lasallistas.org.mx>
2026-08-06 11:37:26 -0600

    Script to keep most of the genes from the original signature

---
+++ common_signature.R
@@ -0,0 +1,41 @@
+
+library(lumi)
+library(illuminaHumanv2.db)
+
+# 1. Convert nuID to ILMN Probe ID
+
+annot$ILMN_ID <- nuID2IlluminaID(as.character(annot$ID), chipVersion = "Human-6 v2")
+
+# The following lines correspond to mapping to ENSEMBL, ENTREZ and SYMBOL from the previous step
+
+
+ensembl_annot <- data.frame(Gene = unlist(mget(
+  x = as.character(annot$ILMN_ID[, 5]) , envir = illuminaHumanv2ENSEMBL
+)))
+
+
+symbol_anot <- data.frame(Gene = unlist(mget(
+  x = as.character(annot$ILMN_ID[, 5]) , envir = illuminaHumanv2SYMBOL
+)))
+
+
+# Convert signature to its ENSEMBL counterpart
+
+pucky_ensembl <- gene_dist$ensembl[gene_dist$gene_name %in% pucky]
+
+# Signature in symbol anottation from probe IDs
+
+pucky[pucky %in% symbol_anot$Gene]
+
+# Signature from GLP
+
+pucky[pucky %in% annot$Symbol]
+
+# Obtaiun gene name in ensembl version of the signature that can be found in thge direct ensembl annot
+
+pucky_ensembl[pucky_ensembl %in% ensembl_annot$Gene] # signature in ensembl annotation
+
+sym_in_EnPucky <- gene_dist$gene_name[gene_dist$ensembl %in% pucky_ensembl[pucky_ensembl %in% ensembl_annot$Gene]] # Obtain symbol form available ensembl
+
+
+common_pucky <- unique(c(sym_in_EnPucky, pucky[pucky %in% annot$Symbol], pucky[pucky %in% as.character(sym_from_entr)]))
