328f816aff423668ceb28339e650288c796c95ef
Pablo-JiFe <pablojimenez@lasallistas.org.mx>
2026-08-06 12:38:38 -0600

    Full counts preprocess, metadata process pending

--- GSE33382_21257.R
+++ GSE33382_21257.R
@@ -1,6 +1,7 @@
 library(oligo)
 library(GEOquery)
 library(dplyr)
+library(tibble)
 library(limma)
 library(hgu133a.db)
 
@@ -10,7 +11,7 @@ library(hgu133a.db)
 
 # getGEOSuppFiles(GEO = "GSE33382", makeDirectory = TRUE, baseDir = "d:/")
 
-untar("D:/GSE33382/GSE33382_RAW.tar", exdir = "d:/GSE33382/")
+
 
 gpl <- getGEO("GPL10295", AnnotGPL = TRUE)
 
@@ -18,32 +19,83 @@ annot <- Table(gpl) %>%
   dplyr::select(ID,
                 Symbol,
                 Illumina_ProbeID,
-                Illumina_Gene)
+                Illumina_Gene,
+                GB_ACC)
 
-gset <- getGEO("GSE21257", GSEMatrix = TRUE)
 
-pheno <- pData(gset[[1]])
+# GSE21257 ----------------------------------------------------------------
 
-unique(pheno$data_processing)
+# Counts
 
-counts_data_norm <- exprs(gset[[1]]) %>% 
+gset_gse21257 <- getGEO("GSE21257", GSEMatrix = TRUE)
+
+pheno_gse21257 <- pData(gset_gse21257[[1]])
+
+unique(pheno_gse21257$data_processing)
+
+counts_data_norm_gse21257 <- exprs(gset_gse21257[[1]]) %>% 
   as.data.frame()
 
-counts_data_norm <- counts_data_norm[rownames(counts_data_norm) %in% annot$ID, ]
+counts_data_norm_gse21257 <- counts_data_norm_gse21257[rownames(counts_data_norm_gse21257) %in% annot$ID, ]
 
-annot <- annot[annot$ID %in% rownames(counts_data_norm), ]
+annot_gse21257 <- annot[annot$ID %in% rownames(counts_data_norm_gse21257), ]
 
-counts_data <- counts_data_norm %>% 
+counts_data_gse21257 <- counts_data_norm_gse21257 %>% 
   rownames_to_column("ID") %>% 
-  left_join(annot, by = "ID") %>% 
-  select(- ID) 
+  left_join(annot_gse21257, by = "ID") %>% 
+  select(- c(ID,
+             Illumina_ProbeID,
+             Illumina_Gene,
+             GB_ACC
+             )) 
 
-counts_data$var <- apply(counts_data[, !(colnames(counts_data) %in% "Symbol")], 1, function(i){var(i, na.rm = TRUE)})
+counts_data_gse21257$var <- apply(counts_data_gse21257[, !(colnames(counts_data_gse21257) %in% "Symbol")], 1, function(i){var(i, na.rm = TRUE)})
 
-counts_data <- counts_data[!is.na(counts_data$Symbol) & counts_data$Symbol != "", ]
+counts_data_gse21257 <- counts_data_gse21257[!is.na(counts_data_gse21257$Symbol) & counts_data_gse21257$Symbol != "", ]
 
-counts_data <- 
-  counts_data %>% 
+counts_data_gse21257 <- 
+  counts_data_gse21257 %>% 
+  group_by(Symbol) %>% 
+  slice_max(order_by = var, n = 1, with_ties = FALSE) %>% 
+  ungroup() %>% 
+  select(- var) %>% 
+  column_to_rownames("Symbol")
+
+# Metadata
+
+
+# GSE33382 ----------------------------------------------------------------
+
+
+gset_gse33382 <- getGEO("GSE33382", GSEMatrix = TRUE)
+
+pheno_gse33382 <- pData(gset_gse33382[[1]])
+
+unique(pheno_gse33382$data_processing)
+
+counts_data_norm_gse33382 <- exprs(gset_gse33382[[1]]) %>% 
+  as.data.frame()
+
+
+counts_data_norm_gse33382 <- counts_data_norm_gse33382[rownames(counts_data_norm_gse33382) %in% annot$ID, ]
+
+annot_gse33382 <- annot[annot$ID %in% rownames(counts_data_norm_gse33382), ]
+
+counts_data_gse33382 <- counts_data_norm_gse33382 %>% 
+  rownames_to_column("ID") %>% 
+  left_join(annot_gse33382, by = "ID") %>% 
+  select(- c(ID,
+             Illumina_ProbeID,
+             Illumina_Gene,
+             GB_ACC
+  )) 
+
+counts_data_gse33382$var <- apply(counts_data_gse33382[, !(colnames(counts_data_gse33382) %in% "Symbol")], 1, function(i){var(i, na.rm = TRUE)})
+
+counts_data_gse33382 <- counts_data_gse33382[!is.na(counts_data_gse33382$Symbol) & counts_data_gse33382$Symbol != "", ]
+
+counts_data_gse33382 <- 
+  counts_data_gse33382 %>% 
   group_by(Symbol) %>% 
   slice_max(order_by = var, n = 1, with_ties = FALSE) %>% 
   ungroup() %>% 
@@ -51,28 +103,4 @@ counts_data <-
   column_to_rownames("Symbol")
 
 
-dist_counts <- get_dist(t(counts_data), method = "manhattan")
-
-# Clustering
-
-hc_counts <- hclust(dist_counts, method = "ward.D2")
-
-
-# Clustering characteristics ------------------------------------------
-
-# Dendrogram
-
-plot(hc_counts,
-     labels = FALSE,
-     hang = -1,
-     main = "CLUSTERING JERÁRQUICO TARGET-OS")
-
-rect.hclust(hc_counts, k = 3, border = "purple") # bottom up approach
-
-# Tree of clusters
-
-clusters <- cutree(hc_counts, k = 2)
-
-sil <- silhouette(clusters, dist_counts)
-
-mean(sil[, "sil_width"])
+# Metadata
