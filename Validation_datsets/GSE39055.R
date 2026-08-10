20a4971e793343394ee1e5965ae090dafd62287c
Mariana Hernández <marianahdez1820@gmail.com>
2026-08-10 01:47:17 -0600

    Refactor probe annotation and update comments
    Updated comments for clarity and improved probe annotation process, including cleaning and handling ambiguous probes.

--- Validation_datsets/GSE39055.R
+++ Validation_datsets/GSE39055.R
@@ -12,8 +12,8 @@ gse39055_directory <- "~/Documents/OSTEOSARCOMA/R.project/Hueso/GSE39055"
 
 # 1.--------------- Load processed expression data -----------------
 
-#NOTE: No normalization step was needed because GSE39055 had already been 
-# normalized using VST and quantile normalization
+#NOTE: No normalization step was needed because GSE39055 was already 
+# normalized with VST and quantile normalization
 
 getGEOSuppFiles("GSE39055", baseDir = gse39055_directory)
 
@@ -23,13 +23,12 @@ length(gse39055)
 
 gse39055_data <- gse39055[[1]]
 
-expr_matrix <- exprs(gse39055_data ) #Extract expression matrix
+expr_matrix <- exprs(gse39055_data) #Extract expression matrix
 
 dim(expr_matrix)
 
 
-# Annotation
-
+# Map Illumina probe ID's to Gene Symbols 
 probe_gene <- AnnotationDbi::select(
   illuminaHumanWGDASLv4.db,
   keys = rownames(expr_matrix),
@@ -37,11 +36,46 @@ probe_gene <- AnnotationDbi::select(
   keytype = "PROBEID"
 )
 
-dim(probe_gene)
-sum(is.na(probe_gene$SYMBOL))
-length(unique(probe_gene$SYMBOL[!is.na(probe_gene$SYMBOL)]))
+# Clean probe annotation
+# Remove probes without a Gene Symbol and remove identical probe-to-gene mappings
+probe_gene_clean <- probe_gene %>%
+  filter(!is.na(SYMBOL), SYMBOL != "") %>%
+  distinct(PROBEID, SYMBOL)
+
+# Identify ambiguous probes that map to more than one Gene Symbol
+ambiguous_probes <- probe_gene_clean %>%
+  group_by(PROBEID) %>%
+  summarise(n_symbols = n_distinct(SYMBOL), .groups = "drop") %>%
+  filter(n_symbols > 1)
+
+# Remove ambiguous probes and keep only probes that map to one Gene Symbol
+probe_gene_clean <- probe_gene_clean %>%
+  group_by(PROBEID) %>%
+  filter(n_distinct(SYMBOL) == 1) %>%
+  dplyr::slice(1) %>%
+  ungroup()
 
 
+# Probe ID to Symbol 
+gene_expression_matrix <- expr_matrix %>%
+  as.data.frame() %>%
+  rownames_to_column("PROBEID") %>% # Move probe ID to column
+  inner_join(probe_gene_clean, by = "PROBEID") %>% # Add Gene Symbolss
+  mutate(variance = apply(dplyr::select(., -PROBEID, -SYMBOL), 1, var)) %>%  # Calculate variance of each probe across all sample
+  group_by(SYMBOL) %>% slice_max(order_by = variance,
+                                 n = 1,
+                                 with_ties = FALSE) %>%
+  ungroup() %>%
+  dplyr::select(-PROBEID, -variance) %>% # Remove unwanted columns
+  column_to_rownames("SYMBOL") %>% # SYMBOL as rowname
+  as.matrix()
+
+dim(gene_expression_matrix)
+head(rownames(gene_expression_matrix))
+anyDuplicated(rownames(gene_expression_matrix))
+sum(is.na(gene_expression_matrix))
+
+  
 
 
 # 2.- Load Metadata GSE39055 ----------------------------------------------
@@ -62,10 +96,26 @@ identical(colnames(expr_matrix), rownames(metadata_gse39055_pre))
 
 metadata_gse39055 <- metadata_gse39055_pre %>%
   mutate(
-    REC_STAT = case_when(`recurrence:ch1` == "Y" ~ 1, `recurrence:ch1` == "N" ~ 0), # 1: recurrence, 0: no recurrence/last follow-up
+    REC_STAT = case_when(`recurrence:ch1` == "Y" ~ 1, `recurrence:ch1` == "N" ~ 0),
+    # 1: recurrence, 0: no recurrence/last follow-up
     REC_MON = as.numeric(`time until first recurrence or latest follow-up (months):ch1`),
     SURV_STAT = case_when(`death:ch1` == "Y" ~ 1, `death:ch1` == "N" ~ 0),
   )
+
+metadata_gse39055 <- metadata_gse39055 %>%
+  mutate(id = geo_accession)
+
+
+identical(colnames(gene_expression_matrix), metadata_gse39055$id)
+
+
+# Match common genes from TARGET-OS signature with GSE39055
+common_genes_TARGET_GSE39055 <- intersect(pucky, rownames(gene_expression_matrix))
+
+length(common_genes_TARGET_GSE39055)
+
+
+
 
 
 
