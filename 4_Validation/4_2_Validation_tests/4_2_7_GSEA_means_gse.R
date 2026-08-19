1d2c65d3055d53c3ffb9ff5d2042c140460688f1
Pablo-JiFe <pablojimenez@lasallistas.org.mx>
2026-08-18 20:35:38 -0600

    create the mean based gsea for gse batch corrected object

---
+++ 4_Validation/4_2_Validation_tests/4_2_7_GSEA_means_gse.R
@@ -0,0 +1,259 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(clusterProfiler)
+library(msigdbr)
+library(aplot)
+library(ggtree)
+
+#############################################################################
+#> Script to perform differential expression analysis on batch corrected
+#> GSE patients utilizing the means of each gene in each cluster. Same as
+#> done on 3_3_GSEA_means.R
+#> 
+#> Steps: 
+##> Calculate mean of each gene of the patients in each cluster (similar to
+##> nearest centroids)
+##> Rank genes based on mean
+##> GSEA on ranked list
+#> 
+#>  
+#> Inputs: metadata_gse33382_for_merge, metadata_gse21257_for_merge, counts_merged
+#> 
+#> Outputs: No outputs used in further analsis
+#> 
+#> Results: 
+##> Objects with GSEA results:
+###> c1_cent_GO
+###> c2_cent_hallmark
+###> c3_cent_GO
+###> c1_cent_hallmark
+###> c2_cent_GO
+###> c3_cent_hallmark
+###> Heatmaps
+#
+#############################################################################
+
+# Load data
+
+
+metadata_gse33382_for_merge <- readRDS("output_data/metadata_gse33382_for_merge.RDS")
+metadata_gse21257_for_merge <- readRDS("output_data/metadata_gse21257_for_merge.RDS")
+
+counts_merged <- readRDS("output_data/counts_merged.RDS")
+
+gene_signature_gse <- scan("output_data/gene_signature_gse.csv", sep = ",", what = character())
+
+metadata_merged <- bind_rows(metadata_gse33382_for_merge,
+                             metadata_gse21257_for_merge)
+
+
+# Scale counts
+
+scaled_counts <- scale(t(counts_merged)[metadata_merged$geo_accession, ])
+
+scaled_counts_df <- as.data.frame(scaled_counts)
+
+all(rownames(scaled_counts_df) == metadata_merged$geo_accession) # Same order
+
+# Asign clusters
+
+scaled_counts_df$clusters <- metadata_merged$clusters # Assign clusters
+
+# Obtain means
+
+cluster_gene_mean <- scaled_counts_df %>%
+  group_by(clusters) %>%
+  summarise(
+    across(everything(), ~ mean(.x, na.rm = TRUE)),
+    .groups = "drop"
+  ) %>%
+  tibble::column_to_rownames("clusters")
+
+# Rank genes
+
+for (i in 1:nrow(cluster_gene_mean)) {
+  
+  rownames(cluster_gene_mean[i,])
+  
+  res <- unlist((cluster_gene_mean[i,]))
+  
+  names(res) <- colnames(cluster_gene_mean)
+  
+  gene_list <- na.omit(sort(res, decreasing = TRUE))
+  
+  # GSEA with GO terms
+  
+  gse <- gseGO(
+    geneList = gene_list, 
+    ont = "ALL", 
+    keyType = "SYMBOL", 
+    pvalueCutoff = 0.01, 
+    OrgDb = "org.Hs.eg.db",
+    minGSSize = 30, 
+    maxGSSize = 100 
+  )
+  
+  
+  gse_df <- as.data.frame(gse)
+  
+  # GSEA with msigdbr
+  
+  msigdbr_collections()
+  
+  m_df <- msigdbr(species = "Homo sapiens", category = "H")
+  
+  msig_t2g <- m_df %>% dplyr::select(gs_name, gene_symbol)
+  
+  set.seed(123) # For reproducibility
+  
+  gsea_res <- GSEA(
+    geneList     = gene_list,
+    TERM2GENE    = msig_t2g,
+    pvalueCutoff = 0.01,
+    pAdjustMethod = "BH",
+    verbose      = FALSE
+  )
+  
+  gsea_df <- as.data.frame(gsea_res)
+  
+  if (i == "1") {
+    
+    # Results for C1
+    
+    c1_cent_GO <- data.frame(row.names = gse$Description, c1  = gse$NES)
+    
+    
+    c1_cent_hallmark <- data.frame(row.names = gsea_res$Description, c1  = gsea_res$NES)
+    
+  } else if (i == "2") {
+    
+    # C2
+    
+    c2_cent_GO <- data.frame(row.names = gse$Description, c2  = gse$NES)
+    
+    
+    c2_cent_hallmark <- data.frame(row.names = gsea_res$Description, c2  = gsea_res$NES)
+    
+    
+    
+  } else if (i == "3") {
+    
+    # C3
+    
+    c3_cent_GO <- data.frame(row.names = gse$Description, c3  = gse$NES)
+    
+    
+    c3_cent_hallmark <- data.frame(row.names = gsea_res$Description, c3  = gsea_res$NES)
+  }
+}
+
+# Keep top and lower 25% of C1 and all of c2 and c3
+
+c1_cent_GO_10 <- c1_cent_GO %>% 
+  filter(c1 > quantile(c1, 0.92) | c1 < quantile(c1, 0.07))
+
+c2_cent_GO_10 <- c2_cent_GO %>% 
+  filter(c2 > quantile(c2, 0.97) | c2 < quantile(c2, 0.12))
+
+c3_cent_GO_10 <- c3_cent_GO %>% 
+  filter(c3 > quantile(c3, 0.9) | c3 < quantile(c3, 0.1))
+
+# Join and convert to matrix
+
+gsea_GO_mat <- merge(c3_cent_GO_10, (merge(c1_cent_GO_10, c2_cent_GO_10, by = 0, all = TRUE) %>% tibble::column_to_rownames("Row.names")), by = 0, all = TRUE)
+
+gsea_GO_mat <- gsea_GO_mat %>% 
+  tibble::column_to_rownames("Row.names") %>% 
+  as.matrix()
+
+gsea_GO_mat[is.na(gsea_GO_mat)] <- 0
+
+# Distances for dendofram
+
+row_hc <- hclust(dist(gsea_GO_mat))
+col_hc <- hclust(dist(t(gsea_GO_mat)))
+
+
+# Heatmap
+
+heatmap_gsea <- gsea_GO_mat %>% 
+  as.data.frame() %>% 
+  tibble::rownames_to_column("path") %>% 
+  pivot_longer(cols = c("c1", "c2", "c3"), names_to = "clusters") %>% 
+  ggplot(aes(x = clusters, y = path, fill = value)) +
+  geom_tile() +
+  scale_fill_distiller(palette = "Spectral", direction = -1) +
+  scale_x_discrete(expand = c(0, 0), labels = c("c1" = "C1", "c3" = "C3", "c2" = "C2")) +  
+  scale_y_discrete(expand = c(0, 0)) +  
+  theme_classic() +
+  labs(x = "Clusters", y = "Pathway", fill = "NES", title = "GSEA from clusters means")
+
+
+# Dendograms
+
+tree_right <- ggtree(row_hc) + 
+  scale_x_reverse() + 
+  scale_y_continuous(expand = c(0, 0))
+
+tree_top <- ggtree(col_hc, hang = -1) + 
+  layout_dendrogram() + 
+  scale_y_reverse(expand = c(0, 0))
+
+# Full heatmap
+
+heatmap_gsea %>% 
+  insert_right(tree_right, width = 0.1) %>% 
+  insert_top(tree_top, height = 0.1)
+
+################################################################################
+
+c1_cent_hallmark_10 <- c1_cent_hallmark %>% 
+  filter(c1 > quantile(c1, 0.75) | c1 < quantile(c1, 0.75))
+
+c2_cent_hallmark_10 <- c2_cent_hallmark %>% 
+  filter(c2 > quantile(c2, 0.75) | c2 < quantile(c2, 0.75))
+
+c3_cent_hallmark_10 <- c3_cent_hallmark %>% 
+  filter(c3 > quantile(c3, 0.75) | c3 < quantile(c3, 0.75))
+
+# Join and convert to matrix
+
+gsea_hallmark_mat <- merge(c3_cent_hallmark_10, (merge(c1_cent_hallmark_10, c2_cent_hallmark_10, by = 0, all = TRUE) %>% tibble::column_to_rownames("Row.names")), by = 0, all = TRUE)
+
+gsea_hallmark_mat <- gsea_hallmark_mat %>% 
+  tibble::column_to_rownames("Row.names") %>% 
+  as.matrix()
+
+gsea_hallmark_mat[is.na(gsea_hallmark_mat)] <- 0
+
+
+row_hc <- hclust(dist(gsea_hallmark_mat))
+col_hc <- hclust(dist(t(gsea_hallmark_mat)))
+
+
+heatmap_gsea_hm <- gsea_hallmark_mat %>% 
+  as.data.frame() %>% 
+  tibble::rownames_to_column("path") %>% 
+  pivot_longer(cols = c("c1", "c2", "c3"), names_to = "clusters") %>% 
+  ggplot(aes(x = clusters, y = path, fill = value)) +
+  geom_tile() +
+  scale_fill_distiller(palette = "Spectral", direction = -1) +
+  scale_x_discrete(expand = c(0, 0), labels = c("c1" = "C1", "c3" = "C3", "c2" = "C2")) +  
+  scale_y_discrete(expand = c(0, 0)) +  
+  theme_classic() +
+  labs(x = "Clusters", y = "Pathway", fill = "NES", title = "GSEA from clusters means")
+
+
+tree_right_hm <- ggtree(row_hc) + 
+  scale_x_reverse() + 
+  scale_y_continuous(expand = c(0, 0))
+
+tree_top_hm <- ggtree(col_hc, hang = -1) + 
+  layout_dendrogram() + 
+  scale_y_reverse(expand = c(0, 0))
+
+heatmap_gsea_hm %>% 
+  insert_right(tree_right_hm, width = 0.1) %>% 
+  insert_top(tree_top_hm, height = 0.1)
+
